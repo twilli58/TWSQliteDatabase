63f31929333948dd3aef48ec173cc1020fba935b
twilli58 <twilli58@my.westga.edu>
2016-02-20 19:00:33 -0500

    added the handler class and the Products class and modified code

---
+++ app/src/main/java/com/ebookfrenzy/database/MyDBHandler.java
@@ -0,0 +1,54 @@
+package com.ebookfrenzy.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Miko on 2/20/2016.
+ */
+public class MyDBHandler extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "productDB.db";
+    public static final String TABLE_PRODUCTS = "products";
+
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_PRODUCTNAME = "productname";
+    public static final String COLUMN_QUANTITY = "quantity";
+
+
+    public MyDBHandler(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, DATABASE_NAME, factory, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_PRODUCTS_TABLE = "CREATE TABLE" +
+                TABLE_PRODUCTS + "("
+                + COLUMN_ID + " INTEGER PRIMARY KEY," + COLUMN_PRODUCTNAME
+                + " TEXT," + COLUMN_QUANTITY + " INTEGER" + ")";
+        db.execSQL(CREATE_PRODUCTS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion,
+                          int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_PRODUCTS);
+        onCreate(db);
+
+    }
+    public void addProduct(Product product) {
+
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_PRODUCTNAME, product.getProductName());
+        values.put(COLUMN_QUANTITY, product.getQuantity());
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        db.insert(TABLE_PRODUCTS, null, values);
+        db.close();
+    }
+
+}
